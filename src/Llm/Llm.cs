1a73c9ff78a20b18b9ee0058959460e8b3ac0afe
ntr <ntr@ihfood.dk>
2024-04-21 13:28:13 +0200

    rename Llm parameters

--- src/Llm/Llm.cs
+++ src/Llm/Llm.cs
@@ -10,11 +10,11 @@ public static partial class Llm
     // B = batch_size, T = sequence_length, C = channels, V = vocab_size
 
     public unsafe static void EncoderForward(float* output,
-                       int* inp, float* wte, float* wpe,
+                       int* input, float* wte, float* wpe,
                        int B, int T, int C)
     {
         // output is (B,T,C). At each position (b,t), a C-dimensional vector summarizing token & position
-        // inp is (B,T) of integers, holding the token ids at each (b,t) position
+        // input is (B,T) of integers, holding the token ids at each (b,t) position
         // wte is (V,C) of token embeddings, short for "weight token embeddings"
         // wpe is (maxT,C) of position embeddings, short for "weight positional embedding"
         for (int b = 0; b < B; b++)
@@ -22,9 +22,9 @@ public static partial class Llm
             for (int t = 0; t < T; t++)
             {
                 // seek to the output position in output[b,t,:]
-                float* out_bt = output + b * T * C + t * C;
-                // get the index of the token at inp[b, t]
-                int ix = inp[b * T + t];
+                float* output_bt = output + b * T * C + t * C;
+                // get the index of the token at input[b, t]
+                int ix = input[b * T + t];
                 // seek to the position in wte corresponding to the token
                 float* wte_ix = wte + ix * C;
                 // seek to the position in wpe corresponding to the position
@@ -32,27 +32,27 @@ public static partial class Llm
                 // add the two vectors and store the result in output[b,t,:]
                 for (int i = 0; i < C; i++)
                 {
-                    out_bt[i] = wte_ix[i] + wpe_t[i];
+                    output_bt[i] = wte_ix[i] + wpe_t[i];
                 }
             }
         }
     }
 
     public unsafe static void EncoderBackward(float* dwte, float* dwpe,
-                          float* dout, int* inp,
+                          float* doutput, int* input,
                           int B, int T, int C)
     {
         for (int b = 0; b < B; b++)
         {
             for (int t = 0; t < T; t++)
             {
-                float* dout_bt = dout + b * T * C + t * C;
-                int ix = inp[b * T + t];
+                float* doutput_bt = doutput + b * T * C + t * C;
+                int ix = input[b * T + t];
                 float* dwte_ix = dwte + ix * C;
                 float* dwpe_t = dwpe + t * C;
                 for (int i = 0; i < C; i++)
                 {
-                    float d = dout_bt[i];
+                    float d = doutput_bt[i];
                     dwte_ix[i] += d;
                     dwpe_t[i] += d;
                 }
@@ -61,11 +61,11 @@ public static partial class Llm
     }
 
     public unsafe static void LayerNormForward(float* output, float* mean, float* rstd,
-                           float* inp, float* weight, float* bias,
+                           float* input, float* weight, float* bias,
                            int B, int T, int C)
     {
         // reference: https://pytorch.org/docs/stable/generated/torch.nn.LayerNorm.html
-        // both inp and output are (B,T,C) of the activations
+        // both input and output are (B,T,C) of the activations
         // mean and rstd are (B,T) buffers, to be used later in backward pass
         // at each position (b,t) of the input, the C-dimensional vector
         // of activations gets normalized, then scaled and shifted
@@ -74,8 +74,8 @@ public static partial class Llm
         {
             for (int t = 0; t < T; t++)
             {
-                // seek to the input position inp[b,t,:]
-                float* x = inp + b * T * C + t * C;
+                // seek to the input position input[b,t,:]
+                float* x = input + b * T * C + t * C;
                 // calculate the mean
                 float m = 0.0f;
                 for (int i = 0; i < C; i++)
@@ -94,12 +94,12 @@ public static partial class Llm
                 // calculate the rstd (reciprocal standard deviation)
                 float s = 1.0f / MathF.Sqrt(v + eps);
                 // seek to the output position in output[b,t,:]
-                float* out_bt = output + b * T * C + t * C;
+                float* output_bt = output + b * T * C + t * C;
                 for (int i = 0; i < C; i++)
                 {
                     float n = (s * (x[i] - m)); // normalize
                     float o = n * weight[i] + bias[i]; // scale and shift
-                    out_bt[i] = o; // write
+                    output_bt[i] = o; // write
                 }
                 // cache the mean and rstd for the backward pass later
                 mean[b * T + t] = m;
@@ -108,17 +108,17 @@ public static partial class Llm
         }
     }
 
-    public unsafe static void LayerNormBackward(float* dinp, float* dweight, float* dbias,
-                            float* dout, float* inp, float* weight, float* mean, float* rstd,
+    public unsafe static void LayerNormBackward(float* dinput, float* dweight, float* dbias,
+                            float* doutput, float* input, float* weight, float* mean, float* rstd,
                             int B, int T, int C)
     {
         for (int b = 0; b < B; b++)
         {
             for (int t = 0; t < T; t++)
             {
-                float* dout_bt = dout + b * T * C + t * C;
-                float* inp_bt = inp + b * T * C + t * C;
-                float* dinp_bt = dinp + b * T * C + t * C;
+                float* doutput_bt = doutput + b * T * C + t * C;
+                float* input_bt = input + b * T * C + t * C;
+                float* dinput_bt = dinput + b * T * C + t * C;
                 float mean_bt = mean[b * T + t];
                 float rstd_bt = rstd[b * T + t];
 
@@ -127,8 +127,8 @@ public static partial class Llm
                 float dnorm_norm_mean = 0.0f;
                 for (int i = 0; i < C; i++)
                 {
-                    float norm_bti = (inp_bt[i] - mean_bt) * rstd_bt;
-                    float dnorm_i = weight[i] * dout_bt[i];
+                    float norm_bti = (input_bt[i] - mean_bt) * rstd_bt;
+                    float dnorm_i = weight[i] * doutput_bt[i];
                     dnorm_mean += dnorm_i;
                     dnorm_norm_mean += dnorm_i * norm_bti;
                 }
@@ -138,75 +138,75 @@ public static partial class Llm
                 // now iterate again and accumulate all the gradients
                 for (int i = 0; i < C; i++)
                 {
-                    float norm_bti = (inp_bt[i] - mean_bt) * rstd_bt;
-                    float dnorm_i = weight[i] * dout_bt[i];
+                    float norm_bti = (input_bt[i] - mean_bt) * rstd_bt;
+                    float dnorm_i = weight[i] * doutput_bt[i];
                     // gradient contribution to bias
-                    dbias[i] += dout_bt[i];
+                    dbias[i] += doutput_bt[i];
                     // gradient contribution to weight
-                    dweight[i] += norm_bti * dout_bt[i];
+                    dweight[i] += norm_bti * doutput_bt[i];
                     // gradient contribution to input
                     float dval = 0.0f;
                     dval += dnorm_i; // term 1
                     dval -= dnorm_mean; // term 2
                     dval -= norm_bti * dnorm_norm_mean; // term 3
                     dval *= rstd_bt; // final scale
-                    dinp_bt[i] += dval;
+                    dinput_bt[i] += dval;
                 }
             }
         }
     }
 
     public unsafe static void MatMulForward(float* output,
-                        float* inp, float* weight, float* bias,
+                        float* input, float* weight, float* bias,
                         int B, int T, int C, int OC)
     {
         // most of the running time is spent here and in matmul_backward
         // OC is short for "output channels"
-        // inp is (B,T,C), weight is (OC, C), bias is (OC)
+        // input is (B,T,C), weight is (OC, C), bias is (OC)
         // output will be (B,T,OC)
         //(int b;
         //#pragma omp parallel for collapse(2)
         Parallel.ForEach(Extensions.Enumerate(B, T), tuple =>
         {
             var (b, t) = tuple;
-            float* out_bt = output + b * T * OC + t * OC;
-            float* inp_bt = inp + b * T * C + t * C;
+            float* output_bt = output + b * T * OC + t * OC;
+            float* input_bt = input + b * T * C + t * C;
             for (int o = 0; o < OC; o++)
             {
                 float val = (bias != null) ? bias[o] : 0.0f;
                 float* wrow = weight + o * C;
                 for (int i = 0; i < C; i++)
                 {
-                    val += inp_bt[i] * wrow[i];
+                    val += input_bt[i] * wrow[i];
                 }
-                out_bt[o] = val;
+                output_bt[o] = val;
             }
         });
     }
 
-    public unsafe static void MatMulBackward(float* dinp, float* dweight, float* dbias,
-                         float* dout, float* inp, float* weight,
+    public unsafe static void MatMulBackward(float* dinput, float* dweight, float* dbias,
+                         float* doutput, float* input, float* weight,
                          int B, int T, int C, int OC)
     {
         // most of the running time is spent here and in matmul_forward
         // this backward could be done in a single "round" of loops
         // but that doesn't afford an efficient parallelization strategy
 
-        // backward into inp first, parallelize over B,T
+        // backward into input first, parallelize over B,T
         //#pragma omp parallel for collapse(2)
         // TODO: Parallize over B+T too not just B
         Parallel.ForEach(Extensions.Enumerate(B, T), tuple =>
         {
             var (b, t) = tuple;
-            float* dout_bt = dout + b * T * OC + t * OC;
-            float* dinp_bt = dinp + b * T * C + t * C;
+            float* doutput_bt = doutput + b * T * OC + t * OC;
+            float* dinput_bt = dinput + b * T * C + t * C;
             for (int o = 0; o < OC; o++)
             {
                 float* wrow = weight + o * C;
-                float d = dout_bt[o];
+                float d = doutput_bt[o];
                 for (int i = 0; i < C; i++)
                 {
-                    dinp_bt[i] += wrow[i] * d;
+                    dinput_bt[i] += wrow[i] * d;
                 }
             }
         });
@@ -218,14 +218,14 @@ public static partial class Llm
             {
                 for (int t = 0; t < T; t++)
                 {
-                    float* dout_bt = dout + b * T * OC + t * OC;
-                    float* inp_bt = inp + b * T * C + t * C;
+                    float* doutput_bt = doutput + b * T * OC + t * OC;
+                    float* input_bt = input + b * T * C + t * C;
                     float* dwrow = dweight + o * C;
-                    float d = dout_bt[o];
+                    float d = doutput_bt[o];
                     if (dbias != null) { dbias[o] += d; }
                     for (int i = 0; i < C; i++)
                     {
-                        dwrow[i] += inp_bt[i] * d;
+                        dwrow[i] += input_bt[i] * d;
                     }
                 }
             }
@@ -233,7 +233,7 @@ public static partial class Llm
     }
 
     public unsafe static void AttentionForward(float* output, float* preatt, float* att,
-                           float* inp,
+                           float* input,
                            int B, int T, int C, int NH)
     {
         // input is (B, T, 3C) holding the query, key, value (Q, K, V) vectors
@@ -252,7 +252,7 @@ public static partial class Llm
         {
             var (b, t, h) = tuple;
 
-            float* query_t = inp + b * T * C3 + t * C3 + h * hs;
+            float* query_t = input + b * T * C3 + t * C3 + h * hs;
             float* preatt_bth = preatt + b * NH * T * T + h * T * T + t * T;
             float* att_bth = att + b * NH * T * T + h * T * T + t * T;
 
@@ -260,7 +260,7 @@ public static partial class Llm
             float maxval = -10000.0f; // TODO something better
             for (int t2 = 0; t2 <= t; t2++)
             {
-                float* key_t2 = inp + b * T * C3 + t2 * C3 + h * hs + C; // +C because it's key
+                float* key_t2 = input + b * T * C3 + t2 * C3 + h * hs + C; // +C because it's key
 
                 // (query_t) dot (key_t2)
                 float val = 0.0f;
@@ -304,27 +304,27 @@ public static partial class Llm
             }
 
             // pass 4: accumulate weighted values into the output of attention
-            float* out_bth = output + b * T * C + t * C + h * hs;
-            for (int i = 0; i < hs; i++) { out_bth[i] = 0.0f; }
+            float* output_bth = output + b * T * C + t * C + h * hs;
+            for (int i = 0; i < hs; i++) { output_bth[i] = 0.0f; }
             for (int t2 = 0; t2 <= t; t2++)
             {
-                float* value_t2 = inp + b * T * C3 + t2 * C3 + h * hs + C * 2; // +C*2 because it's value
+                float* value_t2 = input + b * T * C3 + t2 * C3 + h * hs + C * 2; // +C*2 because it's value
                 float att_btht2 = att_bth[t2];
                 for (int i = 0; i < hs; i++)
                 {
-                    out_bth[i] += att_btht2 * value_t2[i];
+                    output_bth[i] += att_btht2 * value_t2[i];
                 }
             }
         });
     }
 
-    public unsafe static void AttentionBackward(float* dinp, float* dpreatt, float* datt,
-                            float* dout, float* inp, float* att,
+    public unsafe static void AttentionBackward(float* dinput, float* dpreatt, float* datt,
+                            float* doutput, float* input, float* att,
                             int B, int T, int C, int NH)
     {
-        // inp/dinp are (B, T, 3C) Q,K,V
+        // input/dinput are (B, T, 3C) Q,K,V
         // att/datt/dpreatt are (B, NH, T, T)
-        // dout is (B, T, C)
+        // doutput is (B, T, C)
         int C3 = C * 3;
         int hs = C / NH; // head size
         float scale = 1.0f / MathF.Sqrt(hs);
@@ -338,22 +338,22 @@ public static partial class Llm
                     float* att_bth = att + b * NH * T * T + h * T * T + t * T;
                     float* datt_bth = datt + b * NH * T * T + h * T * T + t * T;
                     float* dpreatt_bth = dpreatt + b * NH * T * T + h * T * T + t * T;
-                    float* dquery_t = dinp + b * T * C3 + t * C3 + h * hs;
-                    float* query_t = inp + b * T * C3 + t * C3 + h * hs;
+                    float* dquery_t = dinput + b * T * C3 + t * C3 + h * hs;
+                    float* query_t = input + b * T * C3 + t * C3 + h * hs;
 
                     // backward pass 4, through the value accumulation
-                    float* dout_bth = dout + b * T * C + t * C + h * hs;
+                    float* doutput_bth = doutput + b * T * C + t * C + h * hs;
                     for (int t2 = 0; t2 <= t; t2++)
                     {
-                        float* value_t2 = inp + b * T * C3 + t2 * C3 + h * hs + C * 2; // +C*2 because it's value
-                        float* dvalue_t2 = dinp + b * T * C3 + t2 * C3 + h * hs + C * 2;
+                        float* value_t2 = input + b * T * C3 + t2 * C3 + h * hs + C * 2; // +C*2 because it's value
+                        float* dvalue_t2 = dinput + b * T * C3 + t2 * C3 + h * hs + C * 2;
                         for (int i = 0; i < hs; i++)
                         {
                             // in the forward pass this was:
-                            // out_bth[i] += att_bth[t2] * value_t2[i];
+                            // output_bth[i] += att_bth[t2] * value_t2[i];
                             // so now we have:
-                            datt_bth[t2] += value_t2[i] * dout_bth[i];
-                            dvalue_t2[i] += att_bth[t2] * dout_bth[i];
+                            datt_bth[t2] += value_t2[i] * doutput_bth[i];
+                            dvalue_t2[i] += att_bth[t2] * doutput_bth[i];
                         }
                     }
 
@@ -372,8 +372,8 @@ public static partial class Llm
                     // backward pass 1, the query @ key matmul
                     for (int t2 = 0; t2 <= t; t2++)
                     {
-                        float* key_t2 = inp + b * T * C3 + t2 * C3 + h * hs + C; // +C because it's key
-                        float* dkey_t2 = dinp + b * T * C3 + t2 * C3 + h * hs + C; // +C because it's key
+                        float* key_t2 = input + b * T * C3 + t2 * C3 + h * hs + C; // +C because it's key
+                        float* dkey_t2 = dinput + b * T * C3 + t2 * C3 + h * hs + C; // +C because it's key
                         for (int i = 0; i < hs; i++)
                         {
                             // in the forward pass this was:
@@ -389,46 +389,46 @@ public static partial class Llm
     }
 
     static readonly float GELU_SCALING_FACTOR = MathF.Sqrt(2.0f / MathF.PI);
-    public unsafe static void GeLUForward(float* output, float* inp, int N)
+    public unsafe static void GeLUForward(float* output, float* input, int N)
     {
         // (approximate) GeLU elementwise non-linearity in the MLP block of Transformer
         for (int i = 0; i < N; i++)
         {
-            float x = inp[i];
+            float x = input[i];
             float cube = 0.044715f * x * x * x;
             output[i] = 0.5f * x * (1.0f + MathF.Tanh(GELU_SCALING_FACTOR * (x + cube)));
         }
     }
 
-    public unsafe static void GeLUBackward(float* dinp, float* inp, float* dout, int N)
+    public unsafe static void GeLUBackward(float* dinput, float* input, float* doutput, int N)
     {
         for (int i = 0; i < N; i++)
         {
-            float x = inp[i];
+            float x = input[i];
             float cube = 0.044715f * x * x * x;
             float tanh_arg = GELU_SCALING_FACTOR * (x + cube);
             float tanh_out = MathF.Tanh(tanh_arg);
             float coshf_out = MathF.Cosh(tanh_arg);
             float sech_out = 1.0f / (coshf_out * coshf_out);
             float local_grad = 0.5f * (1.0f + tanh_out) + x * 0.5f * sech_out * GELU_SCALING_FACTOR * (1.0f + 3.0f * 0.044715f * x * x);
-            dinp[i] += local_grad * dout[i];
+            dinput[i] += local_grad * doutput[i];
         }
     }
 
-    public unsafe static void ResidualForward(float* output, float* inp1, float* inp2, int N)
+    public unsafe static void ResidualForward(float* output, float* input1, float* input2, int N)
     {
         for (int i = 0; i < N; i++)
         {
-            output[i] = inp1[i] + inp2[i];
+            output[i] = input1[i] + input2[i];
         }
     }
 
-    public unsafe static void ResidualBackward(float* dinp1, float* dinp2, float* dout, int N)
+    public unsafe static void ResidualBackward(float* dinput1, float* dinput2, float* doutput, int N)
     {
         for (int i = 0; i < N; i++)
         {
-            dinp1[i] += dout[i];
-            dinp2[i] += dout[i];
+            dinput1[i] += doutput[i];
+            dinput2[i] += doutput[i];
         }
     }
 
