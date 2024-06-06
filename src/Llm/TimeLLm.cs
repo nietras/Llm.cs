0fa1d00838a3f3b51e4364855a7b283e756fad3f
nietras <nietras.com@gmail.com>
2024-06-06 21:36:51 +0200

    forward calls

--- src/Llm/TimeLLm.cs
+++ src/Llm/TimeLLm.cs
@@ -8,9 +8,141 @@ namespace nietras.LargeLanguageModel;
 public unsafe class TimeLlm<TLlm>
     where TLlm : ILlm
 {
-    readonly SortedDictionary<TimeKey, List<long>> _keyToTimes = new();
+    readonly SortedDictionary<TimeKey, List<long>> _keyToTimes = [];
 
-    readonly record struct TimeKey(string Part, int Index, string CallerMemberName) : IComparable<TimeKey>
+    public string Part { get; set; } = string.Empty;
+    public int Index { get; set; } = -1;
+
+    Timer NewTimer([CallerMemberName] string callerMemberName = "") =>
+        new(this, callerMemberName);
+
+    public void EmbedForward(
+            int* tokenIndices, float* tokenEmbeddings, float* positionEmbeddings,
+            int batchSize, int tokenCount, int channelCount,
+            float* output)
+    {
+        using var _ = NewTimer();
+        TLlm.EmbedForward(tokenIndices, tokenEmbeddings, positionEmbeddings, batchSize, tokenCount, channelCount, output);
+    }
+    public void EmbedBackward(
+            float* δoutput, int* tokenIndices,
+            int batchSize, int tokenCount, int channelCount,
+            float* δtokenEmbeddings, float* δpositionEmbeddings)
+    {
+        using var _ = NewTimer();
+        TLlm.EmbedBackward(δoutput, tokenIndices, batchSize, tokenCount, channelCount, δtokenEmbeddings, δpositionEmbeddings);
+    }
+
+    public void LayerNormForward(
+            float* input, float* weight, float* bias, int batchSize, int tokenCount, int channelCount,
+            float* mean, float* invStdDev, float* output)
+    {
+        using var _ = NewTimer();
+        TLlm.LayerNormForward(input, weight, bias, batchSize, tokenCount, channelCount, mean, invStdDev, output);
+    }
+    public void LayerNormBackward(
+            float* δoutput, float* input, float* weight, float* mean, float* invStdDev,
+            int batchSize, int tokenCount, int channelCount,
+            float* δweight, float* δbias, float* δinput)
+    {
+        using var _ = NewTimer();
+        TLlm.LayerNormBackward(δoutput, input, weight, mean, invStdDev, batchSize, tokenCount, channelCount, δweight, δbias, δinput);
+    }
+
+    public void MatMulForward(
+            float* input, float* weight, float* bias,
+            int batchSize, int tokenCount, int inputChannelCount, int outputChannelCount,
+            float* output)
+    {
+        using var _ = NewTimer();
+        TLlm.MatMulForward(input, weight, bias, batchSize, tokenCount, inputChannelCount, outputChannelCount, output);
+    }
+    public void MatMulBackward(
+            float* δoutput, float* input, float* weight,
+            int batchSize, int tokenCount, int inputChannelCount, int outputChannelCount,
+            float* δweight, float* δbias, float* δinput)
+    {
+        using var _ = NewTimer();
+        TLlm.MatMulBackward(δoutput, input, weight, batchSize, tokenCount, inputChannelCount, outputChannelCount, δweight, δbias, δinput);
+    }
+
+    public void AttentionForward(
+        float* input,
+        int batchSize, int tokenCount, int channelCount, int headCount,
+        float* preAttention, float* postAttention, float* output)
+    {
+        using var _ = NewTimer();
+        TLlm.AttentionForward(input, batchSize, tokenCount, channelCount, headCount, preAttention, postAttention, output);
+    }
+    public void AttentionBackward(
+        float* δoutput, float* postAttention, float* input,
+        int batchSize, int tokenCount, int channelCount, int headCount,
+        float* δpreAttention, float* δpostAttention, float* δinput)
+    {
+        using var _ = NewTimer();
+        TLlm.AttentionBackward(δoutput, postAttention, input, batchSize, tokenCount, channelCount, headCount, δpreAttention, δpostAttention, δinput);
+    }
+
+    public void GeLUForward(float* input, int count, float* output)
+    {
+        using var _ = NewTimer();
+        TLlm.GeLUForward(input, count, output);
+    }
+    public void GeLUBackward(float* δoutput, float* input, int count, float* δinput)
+    {
+        using var _ = NewTimer();
+        TLlm.GeLUBackward(δoutput, input, count, δinput);
+    }
+
+    public void ResidualForward(float* left, float* right, int count, float* output)
+    {
+        using var _ = NewTimer();
+        TLlm.ResidualForward(left, right, count, output);
+    }
+    public void ResidualBackward(float* δoutput, int count, float* δleft, float* δright)
+    {
+        using var _ = NewTimer();
+        TLlm.ResidualBackward(δoutput, count, δleft, δright);
+    }
+
+    public void SoftmaxForward(float* logits,
+        int batchSize, int tokenCount, int vocabularySize,
+        float* probabilities)
+    {
+        using var _ = NewTimer();
+        TLlm.SoftmaxForward(logits, batchSize, tokenCount, vocabularySize, probabilities);
+    }
+
+    public void CrossEntropyForward(
+        float* probabilities, int* targetTokenIndices,
+        int batchSize, int tokenCount, int vocabularySize,
+        float* losses)
+    {
+        using var _ = NewTimer();
+        TLlm.CrossEntropyForward(probabilities, targetTokenIndices, batchSize, tokenCount, vocabularySize, losses);
+    }
+
+    public void CrossEntropySoftmaxBackward(
+        float* δlosses, float* probabilities, int* targetTokenIndices,
+        int batchSize, int tokenCount, int vocabularySize,
+        float* δlogits)
+    {
+        using var _ = NewTimer();
+        TLlm.CrossEntropySoftmaxBackward(δlosses, probabilities, targetTokenIndices, batchSize, tokenCount, vocabularySize, δlogits);
+    }
+
+
+    public void AdamW(
+        float* gradients, float* ms, float* vs, float* parameters,
+        long parameterCount, float learningRate,
+        float beta1, float beta2, float eps, float weightDecay, int t)
+    {
+        using var _ = NewTimer();
+        TLlm.AdamW(gradients, ms, vs, parameters, parameterCount, learningRate, beta1, beta2, eps, weightDecay, t);
+    }
+
+    readonly record struct TimeKey(string Part, int Index, string CallerMemberName)
+        : IComparable<TimeKey>
     {
         public int CompareTo(TimeKey other)
         {
@@ -45,122 +177,5 @@ public unsafe class TimeLlm<TLlm>
             }
             times.Add(time);
         }
-    }
-
-    public string Part { get; set; } = string.Empty;
-    public int Index { get; set; } = -1;
-
-    Timer NewTimer([CallerMemberName] string callerMemberName = "") =>
-        new(this, callerMemberName);
-
-    public void EmbedForward(
-            int* tokenIndices, float* tokenEmbeddings, float* positionEmbeddings,
-            int batchSize, int tokenCount, int channelCount,
-            float* output)
-    {
-        using var _ = NewTimer();
-        EmbedForward(tokenIndices, tokenEmbeddings, positionEmbeddings, batchSize, tokenCount, channelCount, output);
-    }
-    public void EmbedBackward(
-            float* δoutput, int* tokenIndices,
-            int batchSize, int tokenCount, int channelCount,
-            float* δtokenEmbeddings, float* δpositionEmbeddings)
-    {
-        using var _ = NewTimer();
-        EmbedBackward(δoutput, tokenIndices, batchSize, tokenCount, channelCount, δtokenEmbeddings, δpositionEmbeddings);
-    }
-
-    public void LayerNormForward(
-            float* input, float* weight, float* bias, int batchSize, int tokenCount, int channelCount,
-            float* mean, float* invStdDev, float* output)
-    {
-        using var _ = NewTimer();
-    }
-    public void LayerNormBackward(
-            float* δoutput, float* input, float* weight, float* mean, float* invStdDev,
-            int batchSize, int tokenCount, int channelCount,
-            float* δweight, float* δbias, float* δinput)
-    {
-        using var _ = NewTimer();
-    }
-
-    public void MatMulForward(
-            float* input, float* weight, float* bias,
-            int batchSize, int tokenCount, int inputChannelCount, int outputChannelCount,
-            float* output)
-    {
-        using var _ = NewTimer();
-    }
-    public void MatMulBackward(
-            float* δoutput, float* input, float* weight,
-            int batchSize, int tokenCount, int inputChannelCount, int outputChannelCount,
-            float* δweight, float* δbias, float* δinput)
-    {
-        using var _ = NewTimer();
-    }
-
-    public void AttentionForward(
-        float* input,
-        int batchSize, int tokenCount, int channelCount, int headCount,
-        float* preAttention, float* postAttention, float* output)
-    {
-        using var _ = NewTimer();
-    }
-    public void AttentionBackward(
-        float* δoutput, float* postAttention, float* input,
-        int batchSize, int tokenCount, int channelCount, int headCount,
-        float* δpreAttention, float* δpostAttention, float* δinput)
-    {
-        using var _ = NewTimer();
-    }
-
-    public void GeLUForward(float* input, int count, float* output)
-    {
-        using var _ = NewTimer();
-    }
-    public void GeLUBackward(float* δoutput, float* input, int count, float* δinput)
-    {
-        using var _ = NewTimer();
-    }
-
-    public void ResidualForward(float* left, float* right, int count, float* output)
-    {
-        using var _ = NewTimer();
-    }
-    public void ResidualBackward(float* δoutput, int count, float* δleft, float* δright)
-    {
-        using var _ = NewTimer();
-    }
-
-    public void SoftmaxForward(float* logits,
-        int batchSize, int tokenCount, int vocabularySize,
-        float* probabilities)
-    {
-        using var _ = NewTimer();
-    }
-
-    public void CrossEntropyForward(
-        float* probabilities, int* targetTokenIndices,
-        int batchSize, int tokenCount, int vocabularySize,
-        float* losses)
-    {
-        using var _ = NewTimer();
-    }
-
-    public void CrossEntropySoftmaxBackward(
-        float* δlosses, float* probabilities, int* targetTokenIndices,
-        int batchSize, int tokenCount, int vocabularySize,
-        float* δlogits)
-    {
-        using var _ = NewTimer();
-    }
-
-
-    public void AdamW(
-        float* gradients, float* ms, float* vs, float* parameters,
-        long parameterCount, float learningRate,
-        float beta1, float beta2, float eps, float weightDecay, int t)
-    {
-        using var _ = NewTimer();
     }
 }
