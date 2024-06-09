1f8ef5fb4bde46522a4c879d86c2bac81184133b
nietras <nietras.com@gmail.com>
2024-06-09 20:32:59 +0200

    Try enable Run test again but only for nietras version

--- src/Llm.Test/RunnerTest.cs
+++ src/Llm.Test/RunnerTest.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace nietras.LargeLanguageModel.Test;
@@ -10,14 +8,17 @@ namespace nietras.LargeLanguageModel.Test;
 public class RunnerTest
 {
     public static IEnumerable<object[]> LlmNames { get; } =
-        LlmFactory.NameToCreate.Keys.Select(n => new object[] { n });
+        [[nameof(Llm_nietras)]];
+    //LlmFactory.NameToCreate.Keys.Select(n => new object[] { n });
+
+    public TestContext? TestContext { get; set; }
 
     [TestMethod]
     [DynamicData(nameof(LlmNames))]
     public void RunnerTest_Run(string llmName)
     {
-        if (Environment.ProcessorCount < 8)
-        { Assert.Inconclusive("Skipping Run as otherwise too slow"); }
-        Runner.Run(args: [llmName], dataDirectory: "../../../", t => Trace.WriteLine(t));
+        //if (Environment.ProcessorCount < 8) { Assert.Inconclusive("Skipping Run as otherwise too slow"); }
+        Runner.Run(args: [llmName], dataDirectory: "../../../",
+            t => { TestContext?.WriteLine(t); Trace.WriteLine(t); });
     }
 }
