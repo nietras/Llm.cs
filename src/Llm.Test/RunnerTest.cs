2779da97fae308cca27b29fa8b6fcb9d000c63d9
nietras <nietras@users.noreply.github.com>
2024-06-10 21:38:56 +0200

    Disable code coverage since too slow for RunnerTest (#15)

--- src/Llm.Test/RunnerTest.cs
+++ src/Llm.Test/RunnerTest.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace nietras.LargeLanguageModel.Test;
@@ -8,17 +9,13 @@ namespace nietras.LargeLanguageModel.Test;
 public class RunnerTest
 {
     public static IEnumerable<object[]> LlmNames { get; } =
-        [[nameof(Llm_nietras)]];
-    //LlmFactory.NameToCreate.Keys.Select(n => new object[] { n });
-
-    public TestContext? TestContext { get; set; }
+        LlmFactory.NameToCreate.Keys.Select(n => new object[] { n });
 
     [TestMethod]
     [DynamicData(nameof(LlmNames))]
     public void RunnerTest_Run(string llmName)
     {
-        //if (Environment.ProcessorCount < 8) { Assert.Inconclusive("Skipping Run as otherwise too slow"); }
         Runner.Run(args: [llmName], dataDirectory: "../../../",
-            t => { TestContext?.WriteLine(t); Trace.WriteLine(t); });
+            t => Trace.WriteLine(t));
     }
 }
