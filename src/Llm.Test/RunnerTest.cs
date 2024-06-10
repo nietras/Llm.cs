ca484d337b91dd43d59acf30377696cab2e2a882
nietras <nietras.com@gmail.com>
2024-06-10 21:09:02 +0200

    disable code coverage since too slow when running full train

--- src/Llm.Test/RunnerTest.cs
+++ src/Llm.Test/RunnerTest.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace nietras.LargeLanguageModel.Test;
@@ -9,19 +9,12 @@ namespace nietras.LargeLanguageModel.Test;
 public class RunnerTest
 {
     public static IEnumerable<object[]> LlmNames { get; } =
-        [[nameof(Llm_nietras)]];
-    //LlmFactory.NameToCreate.Keys.Select(n => new object[] { n });
+        LlmFactory.NameToCreate.Keys.Select(n => new object[] { n });
 
-    // Only run in Release since may be too slow in Debug
-#if RELEASE
-    [ExcludeFromCodeCoverage]
     [TestMethod]
     [DynamicData(nameof(LlmNames))]
-#endif
     public void RunnerTest_Run(string llmName)
     {
-        Trace.WriteLine("TEST TEST");
-        //if (Environment.ProcessorCount < 8) { Assert.Inconclusive("Skipping Run as otherwise too slow"); }
         Runner.Run(args: [llmName], dataDirectory: "../../../",
             t => Trace.WriteLine(t));
     }
